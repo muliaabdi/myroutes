7d191f2b71282cfce07b238bc8daab32973ca285
muliaabdi <puntangwine@gmail.com>
2026-02-02 09:59:48 +0700

    feat: Add Google site verification meta tag to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -150,6 +150,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content={siteConfig.name} />
+        <meta name="google-site-verification" content="H56cpUZySaPjJiARR4tNGwCQSXXnGtya9iFHwg_AVpI" />
 
         {/* Structured Data - Organization */}
         <script
